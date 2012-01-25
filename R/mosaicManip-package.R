2e242a94d1b6cf32f4e86dca618f9aee7bbe60ba
Randall Pruim <rpruim@gmail.com>
2012-01-24 21:40:33 -0500

    Keeping NAMESPACE out of git; adding documentation only R files

---
+++ R/mosaicManip-package.R
@@ -0,0 +1,21 @@
+#' mosaicManip
+#'
+#' Manipulatable applets from Project MOSAIC (mosaic-web.org) used to teach mathematics, 
+#' statistics, computation and modeling.  Funded by the NSF, Project MOSAIC is creating a 
+#' community of educators working to tie together aspects of quantitative work that students 
+#' in science, technology, engineering and mathematics will need in their professional lives, 
+#' but which are usually taught in isolation, if at all.
+#'
+#'
+#' @name mosaicManip-package
+#' @aliases mosaic mosaicManip-package
+#' @docType package
+#' @title mosaicManip: the Project MOSAIC manipulatable applets package
+#' @author Randall Pruim (\email{rpruim@@calvin.edu}), Daniel Kaplan (\email{kaplan@@macalester.edu})
+#' @references
+#' \url{http://mosaic-web.org}
+#'
+#' @keywords package
+#' @importFrom utils head modifyList
+#' @exportPattern "^[^\\.]"
+NA
---
+++ R/undocumented.R
@@ -0,0 +1,15 @@
+#' Undocumented Objects
+#' 
+#' Documentation is still pending for these objects
+#' 
+#' 
+#' @name undocumented
+#' @rdname undocumented
+#' @aliases mEulerIntegrate mGlassPattern mGradSearch mLM mLinAlgebra mLineFit
+#' mPhaseLine mhistogram mpoly2
+#' @author Randall Pruim, Daniel Kaplan
+#' 
+NULL
+
+
+
